86edda2bcf43946db088f82205379fb0f8e85290
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-12-01 17:40:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include "lib.h"
+
 using namespace std;
 
 int main() {
-    int c;
-cin>>c;
-if (primo(c,c)){
-    cout<<"numero primo";
-}else{
-    cout<<"numero non primo";
-}
+    int c = 0;
+    cin >> c;
+    if(primo(c,c)){
+        cout << "numero primo";
+    }else{
+        cout << "numero non primo";
+    }
     return 0;
 }
